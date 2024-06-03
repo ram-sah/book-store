c6f842022543d2535331258c554c25321a09e904
ram-sah <mohan2036@gmail.com>
2024-06-03 10:42:56 -0400

    deployment

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +9,10 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 });
