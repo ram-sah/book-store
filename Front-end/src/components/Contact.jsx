564c22a0afa0e04ac9df36c939a91dce80faa7e6
ram-sah <mohan2036@gmail.com>
2024-06-04 14:39:02 -0400

    theme update

--- Front-end/src/components/Contact.jsx
+++ Front-end/src/components/Contact.jsx
@@ -4,23 +4,23 @@ const Contact = () => {
     return (
         <>
             <div className=' max-w-screen-2xl mx-auto my-40 flex flex-col justify-center items-center dark:text-black '>
-                <div className='w-96 flex flex-col gap-5 py-5 px-10 rounded-2xl shadow-2xl shadow-red-200 drop-shadow-2xl dark:bg-slate-100' >
+                <div className='w-96 flex flex-col gap-5 py-5 px-10 rounded-2xl shadow-2xl shadow-red-200 drop-shadow-2xl dark:bg-slate-500' >
                     <h1 className='text-center font-bold text-2xl my-10'>Contact Us</h1>
-                    <label className=" input flex items-center gap-2">
+                    <label className=" input flex items-center gap-2 dark:bg-slate-200">
                         Name:
                         <input type="text" className="grow" placeholder="Enter your name" />
                     </label>
-                    <label className="input flex items-center gap-2">
+                    <label className="input flex items-center gap-2 dark:bg-slate-200">
                         Email:
                         <input type="text" className="grow" placeholder="Enter your email" />
                     </label>
-                    <label className="input flex items-center gap-2">
+                    <label className="input flex items-center gap-2 dark:bg-slate-200">
                         Phone:
                         <input type="tel" className="grow" placeholder="Enter your Phone" />
                     </label>
-                    <textarea className=" textarea" placeholder="Enter your message"></textarea>
+                    <textarea className=" textarea dark:bg-slate-200" placeholder="Enter your message"></textarea>
                     <div className='pt-5 pb-10 text-center'>
-                        <button type='submit' className='px-3 py-2 bg-pink-500 hover:bg-pink-800 rounded-md text-white'>Submit</button>
+                        <button type='submit' className='px-3 py-2 bg-pink-500 hover:bg-pink-800 rounded-md text-white '>Submit</button>
                     </div>
                 </div>
             </div>
